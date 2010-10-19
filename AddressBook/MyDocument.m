84c8725f03ce0fc5e48afd78d7f43a2c557a394c
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2010-10-19 03:20:29 +0000

    Adding the AddressBook project by André

---
+++ AddressBook/MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  AddressBook
+//
+//  Created by André Renault on 10/17/10.
+//  Copyright KompileSoft 2010 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
