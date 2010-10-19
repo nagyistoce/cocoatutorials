e79315eb7b8da598df904e90e86f4141ad0f146c
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2010-10-19 03:20:29 +0000

    Adding the AddressBook project by André

---
+++ trunk/AddressBook/MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  AddressBook
+//
+//  Created by André Renault on 10/17/10.
+//  Copyright KompileSoft 2010 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
