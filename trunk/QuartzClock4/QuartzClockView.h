ed633de3d505072ffb816a702bf3f2c6404accdd
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2011-04-05 06:52:27 +0000

    Initial version of QuartzClock4

---
+++ trunk/QuartzClock4/QuartzClockView.h
@@ -0,0 +1,55 @@
+//
+//  QuartzClockView.h
+//  This file is part of QuartzClock
+// 
+//  QuartzClock is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  QuartzClock is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with QuartzClock.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  Written by Robin Mills, San Jose, CA, USA. 2010 http://clanmills.com
+//
+
+#import <Cocoa/Cocoa.h>
+@interface QuartzClockView : NSView
+{
+	
+	// properties
+	NSTimer*			timer;
+	QuartzClockView*	myIcon;
+	BOOL				isDocked;
+	BOOL				bCmdKey;
+	NSPoint				initialLocation;
+    NSSize              initialSize;
+}
+
+// Class methods
++ (NSUInteger) borderMask ;
++ (NSUInteger) borderNone ;
++ (NSUInteger) borderToggle : (NSUInteger) now;
+
+// actions
+- (IBAction) saveAs       : (id) sender;
+- (IBAction) setTitleNow  : (id) sender;
+- (IBAction) borderToggle : (id) sender;
+- (IBAction) faceToggle   : (id) sender;
+
+// property getter/setters
+@property BOOL    isDocked;
+@property NSPoint initialLocation; // position of mouse at mouse down
+@property NSSize  initialSize;     // size of window at mouse down
+
+// methods
+- (void) stopClockUpdates;
+- (void) startClockUpdates;
+
+@end
+
