b4823eb0741e83affd7db076056727ef1f14aef4
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2011-09-14 06:50:19 +0000

    BYOB4 - XCode4 version of BYOB tutorial

---
+++ BYOB4/BYOB4/BYOB4/BYOB4AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  BYOB4AppDelegate.m
+//  BYOB4
+//
+//  Created by Robin Mills on 2011-09-13.
+//  Copyright 2011 RMSC. All rights reserved.
+//
+
+#import "BYOB4AppDelegate.h"
+
+@implementation BYOB4AppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
