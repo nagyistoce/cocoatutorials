41d6604b6e983d757b9cd8890fe3eecd73c369c4
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2011-09-14 06:50:19 +0000

    BYOB4 - XCode4 version of BYOB tutorial

---
+++ trunk/BYOB4/BYOB4/BYOB4/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BYOB4
+//
+//  Created by Robin Mills on 2011-09-13.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
