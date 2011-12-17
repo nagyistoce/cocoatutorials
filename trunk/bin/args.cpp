f9e7e3649f7cb4864db3c2f5cdf2188c2e52ed61
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2011-12-17 05:57:40 +0000

    Removed trunk/bin

--- trunk/bin/args.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, char* argv[])
-{
-	int i = 0 ;
-	while ( i++ < argc ) printf("%-2d: %s\n",i,argv[i-1]) ;
-    return 0 ;
-}
--- trunk/bin/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/csh
-
-foreach lib ( libChar16.dylib libCoreTypes.dylib libCrypto.dylib libSSL.dylib libcurl.3.0.0.dylib )
-	echo 'lib = ' $lib
-end
-
-
