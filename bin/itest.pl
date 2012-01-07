29826c46ef44dc8d51291e6d9957beac7f902765
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2012-01-07 16:20:50 +0000

    Get itest.pl to accept multiple files from command-line

--- bin/itest.pl
+++ bin/itest.pl
@@ -2,13 +2,22 @@
 
 use Imager;
 
+sub println
+{
+	my $s = '';
+	for my $a (@_) {
+		$s .= $a . ' ';
+	}
+	print($s,"\n");
+}
+
+
 my $image = Imager->new;
+for my $source (@ARGV) {
+	$image->read(file=>$source) or die "Cannot load $image_source: ", $image->errstr;
+	println("source = $source", "width =", $image->getwidth , "height =" , $image->getheight);
+}
 
-my $source = shift; # from the command-line
-$image->read(file=>$source)
-    or die "Cannot load $image_source: ", $image->errstr;
-    
-print ("width = " . $image->getwidth . " height = " . $image->getheight . "\n");
-
+1;
 # That's all Folks!
 ##
