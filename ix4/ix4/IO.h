d7b350b3c53373b35a905072e0eb8c30dcd4eccc
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2012-02-06 06:41:52 +0000

    Work in Progress Refactored code to put PDF and IO processing into different files Now working with ddcli argument parser.

---
+++ ix4/ix4/IO.h
@@ -0,0 +1,24 @@
+//
+//  IO.h
+//  ix4
+//
+//  Created by Robin Mills on 2012-02-05.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef ix4_IO_h
+#define ix4_IO_h
+
+void setVerbose(boolean_t verbose);
+boolean_t getVerbose();
+
+// report => stdout error => stderr warn => stderr IF verbose
+void warn    (const char* msg);
+void errors  (const char* msg);
+void report  (const char* msg);
+
+void warns   (NSString* msg);
+void errorns (NSString* msg);
+void reportns(NSString* msg);
+
+#endif
