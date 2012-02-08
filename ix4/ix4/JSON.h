35ecc6177a9bd801c919b5c698a279bf0fb570c3
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2012-02-08 04:51:49 +0000

    Added metadata support by calling ExifTool on images to create JSON Added JSON support to read the metadata Refactored the sort and label code for simpler integration of metadata support

---
+++ ix4/ix4/JSON.h
@@ -0,0 +1,15 @@
+//
+//  JSON.h
+//  ix4
+//
+//  Created by Robin Mills on 2012-02-07.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSON : NSObject
+
+-(NSObject*) jsonToObj:(NSString *)json ;
+-(NSObject*) jsonToObj:(NSString *)json offset:(int)offset nextOffset:(int*)nextOffset;
+@end
