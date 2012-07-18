d2996d44687d48726cf5ba0f9226530f6f8773dc
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2012-07-18 04:59:15 +0000

    Updating timestamp to do ascii -> number (as well as number->ascii)

--- bin/timestamp.cpp
+++ bin/timestamp.cpp
@@ -12,16 +12,55 @@ int syntax()
 	return 1 ;
 }
 
+//559 rmills@rmills-imac:~/bin $ exiv2 -pa ~/R.jpg | grep -i date
+//Exif.Image.DateTime                          Ascii      20  2009:08:03 08:58:57
+//Exif.Photo.DateTimeOriginal                  Ascii      20  2009:08:03 08:58:57
+//Exif.Photo.DateTimeDigitized                 Ascii      20  2009:08:03 08:58:57
+//Exif.GPSInfo.GPSDateStamp                    Ascii      21  2009-08-03T15:58:57Z
+
 int main(int argc,char* argv[])
 {
 	if ( argc < 2  ) return syntax() ;
 
-	int 	arg = 1 ;
-	while ( arg < argc )
+	int 	 a = 1 ;
+	while (  a < argc )
 	{
-		unsigned int time  = (long) atoi(argv[arg++]) ;
+		char* arg = argv[a++];
+		unsigned int time = 999 ;
+		if ( strstr(arg,":") || strstr(arg,"-") ) {
+			int  Y,M,D,H,m,S ;
+			char a,b,c,d,e,f ;
+			printf("parsing %s\n",arg);
+			sscanf(arg,"%d%c%d%c%d%c%d%c%d%c%d",&Y,&a,&M,&b,&D,&d,&H,&e,&m,&f,&S);
+			printf("date = %02d-%02d-%02d  time = %02d:%02d:%02d\n",Y,M,D,H,m,S);
+			
+			struct tm T;
+#if 0
+		    int tm_sec;     /* seconds (0 - 60) */
+            int tm_min;     /* minutes (0 - 59) */
+            int tm_hour;    /* hours (0 - 23) */
+            int tm_mday;    /* day of month (1 - 31) */
+            int tm_mon;     /* month of year (0 - 11) */
+            int tm_year;    /* year - 1900 */
+            int tm_wday;    /* day of week (Sunday = 0) */
+            int tm_yday;    /* day of year (0 - 365) */
+            int tm_isdst;   /* is summer time in effect? */
+            char *tm_zone;  /* abbreviation of timezone name */
+            long tm_gmtoff; /* offset from UTC in seconds */
+#endif
+			memset(&T,sizeof(T),0);
+			T.tm_min  = m  ;
+			T.tm_hour = H  ;
+			T.tm_sec  = S  ;
+			T.tm_year = Y -1900 ;
+			T.tm_mon  = M -1    ;
+			T.tm_mday = D  ;
+			time = mktime(&T);
+		} else {
+			time  = (long) atoi(arg) ;
+		}
 		time_t clock = (clock_t) time           ;
-		printf("%u = %s",time,ctime(&clock))    ;
+		printf("%u = %s\n",time,ctime(&clock))    ;
 	}
 
 	return 0 ;
