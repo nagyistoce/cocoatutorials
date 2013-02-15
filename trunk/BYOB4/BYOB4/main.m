bde34aa4d1633a3afba9844413e76c7870120540
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-02-15 06:43:49 +0000

    All new version of BYOB4.  Documentation at http://clanmills.com/files/BYOB4.pdf

---
+++ trunk/BYOB4/BYOB4/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BYOB4
+//
+//  Created by Robin Mills on 13-2-9.
+//  Copyright (c) 2013 Robin Mills. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
