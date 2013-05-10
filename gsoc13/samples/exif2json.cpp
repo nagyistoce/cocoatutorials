fe63fa2da71551b9d941b2732832c5f4989329ed
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-05-10 06:41:02 +0000

    JSON: exif2json.cpp final polish.  Got to get on with libwebsockets.

--- gsoc13/samples/exif2json.cpp
+++ gsoc13/samples/exif2json.cpp
@@ -21,9 +21,10 @@ using namespace Jzon;
 using namespace Exiv2;
 
 // http://stackoverflow.com/questions/236129/splitting-a-string-in-c
-static size_t split(const std::string &s, char delim, std::vector<std::string> &elems) {
+static size_t split(const std::string& s, char delim, std::vector<std::string>& elems)
+{
     std::stringstream ss(s);
-    std::string item;
+    std::string       item;
     while (std::getline(ss, item, delim)) {
         elems.push_back(item);
     }
