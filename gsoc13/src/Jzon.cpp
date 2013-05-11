dca332cde27116b8fb64a3bc03424e82ebcebb8f
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-05-11 20:36:53 +0000

    JSON: modify Jzon.{cpp|h} to be have no reference to anything in Exiv2.

--- gsoc13/src/Jzon.cpp
+++ gsoc13/src/Jzon.cpp
@@ -19,6 +19,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+
+#ifdef  _WINDLL
+#define JzonAPI __declspec(dllexport)
+#endif
+
 #include "Jzon.h"
 
 #include <sstream>
