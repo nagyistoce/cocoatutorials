0bef25f37e6a53abf8577e5156aeb6d7afc526b6
nhudinhtuan@gmail.com <nhudinhtuan@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-05-12 16:42:34 +0000

    HTTP with switch between Exiv2_HTTP & libcurl. No proxy, No requestHeader, output has responseHeaders.

---
+++ gsoc13/demo/http.hpp
@@ -0,0 +1,32 @@
+#ifndef HTTP_H_
+#define HTTP_H_
+
+#include <string>
+#include <map>
+#include <algorithm>    // find
+
+#include <exiv2/exiv2.hpp>
+
+namespace Exiv2 {
+    typedef std::map<std::string,std::string> dict_t;
+    typedef dict_t::iterator                  dict_i;
+
+    EXIV2API int http(dict_t& request,dict_t& response,std::string& errors);
+
+    // http://stackoverflow.com/questions/2616011/easy-way-to-parse-a-url-in-c-cross-platform
+    class Uri
+    {
+    public:
+	    std::string QueryString;
+	    std::string Path;
+	    std::string Protocol;
+	    std::string Host;
+	    std::string Port;
+
+	    static Uri EXIV2API Parse(const std::string &uri);
+    };
+}
+
+#endif
+
+
---
+++ gsoc13/demo/httptest.cpp
@@ -0,0 +1,75 @@
+/*
+ http.cpp
+ */
+
+#include "http.hpp"
+#include <iostream>
+using namespace std;
+
+int main(int argc,const char** argv)
+{
+    if ( argc < 3 ) {
+        cout << "usage  : " << argv[0] << " [key value]+" << endl;
+        cout << "example: " << argv[0] << " [-url url | -server clanmills.com -page /LargsPanorama.jpg] -header \"Range: bytes=0-200\"" << endl;
+		cout << "or     : " << argv[0] << " -url http://clanmills.com/robin.shtml" << endl;
+        cout << "useful  keys: -verb {GET|HEAD|PUT}  -page str -server str -port number -version [-header something]+ " << endl;
+        cout << "default keys: -verb GET -server clanmills.com -page robin.shtml -port 80 -version 1.0" << endl;
+        cout << "export http_proxy=url eg http_proxy=http://64.62.247.244:80" << endl;
+        return 0;
+    }
+
+    Exiv2::dict_t response;
+    Exiv2::dict_t request;
+    string errors;
+
+    request["page"  ]   = "robin.shtml";
+    request["server"]   = "clanmills.com";
+
+    // convert the command-line arguments into the request dictionary
+    for ( int i = 1 ; i < argc-1 ; i +=2 ) {
+        const char* arg = argv[i];
+        // skip past the -'s on the key
+        while ( arg[0] == '-' ) arg++;
+
+        if ( string(arg) == "header" ) {
+            string header = argv[i+1];
+            if ( ! strchr(argv[i+1],'\n') ) {
+                header += "\r\n";
+            }
+            request[arg] += header;
+        } else if ( string(arg) == "uri" || string(arg) == "url" ) {
+        	Exiv2::Uri uri = Exiv2::Uri::Parse(argv[i+1]);
+        	if ( uri.Protocol == "http" ) {
+        	    request["server"] = uri.Host;
+        	    request["page"]   = uri.Path;
+        	    request["port"]   = uri.Port;
+        	}
+        } else {
+            request[arg]=argv[i+1];
+        }
+    }
+
+    int result = Exiv2::http(request,response,errors);
+    cout << "result = " << result << endl;
+    cout << "errors = " << errors << endl;
+    cout << endl;
+
+    for ( Exiv2::dict_i it = response.begin() ; it != response.end() ; it++ ) {
+        cout << it->first << " -> ";
+
+        if ( it->first ==  "body") {
+        	string& value = it->second;
+        	cout << "# " << value.length();
+        	if ( value.length() < 1000 ) cout << " = " << value ;
+        } else {
+        	cout << it->second;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// That's all Folks!
+////
