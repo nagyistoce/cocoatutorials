16050f94c2999012ba738e54d2ea3fe66d24e5cc
nhudinhtuan@gmail.com <nhudinhtuan@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-06-20 12:14:51 +0000

    test script for SSH IO

--- gsoc13/test/sshiotest.sh
+++ gsoc13/test/sshiotest.sh
@@ -5,7 +5,7 @@ source ./functions.source
 
 ##
 #function to test remote read access in basicio.cpp
-SshReadTest()
+SshIOTest()
 {
     arg=$1
     scheme=${arg:0:4}
@@ -13,10 +13,16 @@ SshReadTest()
     src=$(basename "$arg")
     filename=${src%.*}
     test=${filename}.txt
-    good=$datapath/${filename}.txt
+    good=$datapath/remoteio.txt
+    cmdadd=$datapath/cmdremoteadd.txt
+    cmddel=$datapath/cmdremotedel.txt
     dot=.
-    # run tests
-    runTest exifprint $1 > $test
+    # add/set metadata
+    runTest exiv2 "-m" $cmdadd $1
+    # print out the metadata
+    runTest exifprint $1 "--curl" > $test
+    # delete metadata
+    runTest exiv2 "-m" $cmddel $1
     #check results
     diffCheckAscii $test $good
 
@@ -40,15 +46,19 @@ SshReadTest()
     fi
 
     errors=0
-    files+=(remoteImg{0..9}.jpg)
+    iopngfiles+=(remoteio{1..5}.png)
+    iojpgfiles+=(remoteio{6..10}.jpg)
     USE_SSH=$("$bin"/exiv2 -v -V | grep ^ssh= | sed s/ssh=//)
     if [ "$USE_SSH" == "0" ]; then
         #Skip remoteIo test cases
         echo 'Ssh is not used. Skip sshio test cases.'
     else
-        printf 'SSH READ '
-        for name in ${files[@]}; do
-            SshReadTest "ssh://$EXIV2_AWSUBUNTU_USERNAME:$EXIV2_AWSUBUNTU_PASSWORD@$EXIV2_AWSUBUNTU_HOST/www/$name"
+        printf 'SSH IO '
+        for name in ${iopngfiles[@]}; do
+            SshIOTest "ssh://$EXIV2_AWSUBUNTU_USERNAME:$EXIV2_AWSUBUNTU_PASSWORD@$EXIV2_AWSUBUNTU_HOST/www/$name"
+        done
+        for name in ${iojpgfiles[@]}; do
+            SshIOTest "ssh://$EXIV2_AWSUBUNTU_USERNAME:$EXIV2_AWSUBUNTU_PASSWORD@$EXIV2_AWSUBUNTU_HOST/www/$name"
         done
         if [ $errors -eq 0 ]; then
             printf '\nAll test cases passed\n'
