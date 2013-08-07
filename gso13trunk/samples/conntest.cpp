0cde0ac34bb58e8f6c2b1afac6028ed4ef662dd5
nhudinhtuan@gmail.com <nhudinhtuan@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-08-07 07:23:08 +0000

    specify http1.0 or http1.1 in conntest

--- gso13trunk/samples/conntest.cpp
+++ gso13trunk/samples/conntest.cpp
@@ -16,7 +16,7 @@
 #include <stdlib.h>
 using namespace std;
 
-void httpcon(const std::string& url) {
+void httpcon(const std::string& url, bool useHttp1_0 = false) {
     Exiv2::dict_t response;
     Exiv2::dict_t request;
     string        errors;
@@ -27,6 +27,7 @@ void httpcon(const std::string& url) {
     request["server"] = uri.Host;
     request["page"]   = uri.Path;
     request["port"]   = uri.Port;
+    if (!useHttp1_0) request["version"] = "1.1";
 
     int serverCode = Exiv2::http(request,response,errors);
     if (serverCode < 0 || serverCode >= 400 || errors.compare("") != 0) {
@@ -35,7 +36,7 @@ void httpcon(const std::string& url) {
 }
 
 #if EXV_USE_CURL == 1
-void curlcon(const std::string& url) {
+void curlcon(const std::string& url, bool useHttp1_0 = false) {
     CURL* curl = curl_easy_init();
     if(!curl) {
         throw Exiv2::Error(1, "Uable to init libcurl.");
@@ -55,6 +56,9 @@ void curlcon(const std::string& url) {
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
     curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, timeout);
+    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1); // debug
+    if (useHttp1_0) curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+    else            curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 
     /* Perform the request, res will get the return code */
     CURLcode res = curl_easy_perform(curl);
@@ -70,8 +74,6 @@ void curlcon(const std::string& url) {
     if (returnCode >= 400 || returnCode < 0) {
         throw Exiv2::Error(55, "Server", returnCode);
     }
-
-
 }
 #endif
 
@@ -118,25 +120,39 @@ void sftpcon(const std::string& url) {
 int main(int argc,const char** argv)
 {
     if (argc < 2) {
-        cout << "Usage: " << argv[0] << " url\n\n";
+        cout << "Usage: " << argv[0] << " url {-http1_0}\n";
         return 1;
     }
     std::string url(argv[1]);
     Exiv2::Protocol prot = Exiv2::fileProtocol(url);
 
+    bool useHttp1_0 = false;
+    for ( int a = 1 ; a < argc ; a++ ) {
+        std::string arg(argv[a]);
+        if (arg == "-http1_0") useHttp1_0 = true;
+    }
+
     bool isOk = false;
     try {
         #if EXV_USE_SSH == 1
-            if (prot == Exiv2::pSsh) {sshcon(url); isOk = true;}
-            else if (prot == Exiv2::pSftp){sftpcon(url); isOk = true;}
-        #endif
-        #if EXV_USE_CURL == 1
-            if (prot == Exiv2::pHttp || prot == Exiv2::pHttps || prot == Exiv2::pFtp) {
-                curlcon(url);
+            if (prot == Exiv2::pSsh) {
+                sshcon(url);
+                isOk = true;
+            } else if (prot == Exiv2::pSftp){
+                sftpcon(url);
                 isOk = true;
             }
         #endif
-        if (!isOk && prot == Exiv2::pHttp){ httpcon(url); isOk = true;}
+        #if EXV_USE_CURL == 1
+            if (prot == Exiv2::pHttp || prot == Exiv2::pHttps || prot == Exiv2::pFtp) {
+                curlcon(url, useHttp1_0);
+                isOk = true;
+            }
+        #endif
+        if (!isOk && prot == Exiv2::pHttp) {
+            httpcon(url, useHttp1_0);
+            isOk = true;
+        }
     } catch (Exiv2::AnyError& e) {
         std::cout << "Error: '" << e << "'\n";
         return -1;
