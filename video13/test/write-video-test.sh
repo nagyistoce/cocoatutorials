90b991ac9310e8a8e2632ed194ea560fd022f2a0
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2013-09-27 04:29:08 +0000

    test: modified write-vide-test.sh to use bash arrays.

--- video13/test/write-video-test.sh
+++ video13/test/write-video-test.sh
@@ -1,95 +1,85 @@
 #!/bin/bash
-# Test driver for video files
+# Test driver for write video
 
 source ./functions.source
 
-echo "Creating temporary directory modifiedvideo to test Video Metadata Read-Write functionality" 
-
-videos=$testdir/video*
-
-#copy all test Video files to temporary Directory
-cd $testdir
-for file in ../data/video/video-*; do
-	 video="`basename "$file"`"
-		if [ $video != "video-test.out" ] ; then
-			copyTestFile "video/$video" "$video"
-		fi
-done
-
-#Write all Metadata to the files
-runTest exiv2 -M "set Xmp.video.MicroSecPerFrame 64" $videos
-runTest exiv2 -M "set Xmp.video.MaxDataRate 4096" $videos
-runTest exiv2 -M "set Xmp.video.FrameCount 2048" $videos
-runTest exiv2 -M "set Xmp.video.InitialFrames 4" $videos
-runTest exiv2 -M "set Xmp.video.StreamCount 2" $videos
-runTest exiv2 -M "set Xmp.video.SuggestedBufferSize 1024" $videos
-runTest exiv2 -M "set Xmp.video.Width 240" $videos
-runTest exiv2 -M "set Xmp.video.Height 320" $videos
-runTest exiv2 -M "set Xmp.video.FileDataRate 128" $videos
-runTest exiv2 -M "set Xmp.video.Duration 2048" $videos
-runTest exiv2 -M "set Xmp.video.Codec mjpg" $videos
-runTest exiv2 -M "set Xmp.video.FrameRate 1024" $videos
-runTest exiv2 -M "set Xmp.video.VideoQuality 128" $videos
-runTest exiv2 -M "set Xmp.video.VideoSampleSize 256" $videos
-runTest exiv2 -M "set Xmp.audio.Codec mpv4" $videos
-runTest exiv2 -M "set Xmp.audio.SampleRate 32" $videos
-runTest exiv2 -M "set Xmp.audio.SampleCount 32" $videos
-runTest exiv2 -M "set Xmp.video.DateUT $date" $videos
-runTest exiv2 -M "set Xmp.video.Comment Metadata was Edited Using Exiv2" $videos
-runTest exiv2 -M "set Xmp.video.Language Kannada" $videos
-runTest exiv2 -M "set Xmp.video.Country India" $videos
-runTest exiv2 -M "set Xmp.video.Copyright Photographer" $videos
-runTest exiv2 -M "set Xmp.video.Genre Sample Test Video" $videos
-runTest exiv2 -M "set Xmp.video.Software Exiv2 0.25" $videos
-runTest exiv2 -M "set Xmp.video.Junk Its a junk Data" $videos
-runTest exiv2 -M "set Xmp.video.MediaLanguage English" $videos
-
-echo "Metadata has been written to the File"
+##
+# set up the output file
+out=$(real_path "$testdir/$this.out")
+copyTestFile "video/$this.out"
 
 (	cd "$testdir"
 
-    for file in video-*; do
-        video="`basename "$file"`"
-	if [ $video != "video-test.out" ] ; then
+	##
+	# find video files data/video and copy them for testing
+	declare -a videos
+	for video in $datadir/video/* ; do
+		# http://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+		ext="${video##*.}"
+		if [ $ext != out ]; then
+			copyTestFile "$video" 
+			videos+=($(basename "$video"))
+		fi
+	done 
 
-	        printf "." >&3
+	# write metadata to videos
+	runTest exiv2 -M "set Xmp.video.MicroSecPerFrame 64"      ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.MaxDataRate 4096"         ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.FrameCount 2048"          ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.InitialFrames 4"          ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.StreamCount 2"            ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.SuggestedBufferSize 1024" ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Width 240"                ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Height 320"               ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.FileDataRate 128"         ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Duration 2048"            ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Codec mjpg"               ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.FrameRate 1024"           ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.VideoQuality 128"         ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.VideoSampleSize 256"      ${videos[*]}
+	runTest exiv2 -M "set Xmp.audio.Codec mpv4"               ${videos[*]}
+	runTest exiv2 -M "set Xmp.audio.SampleRate 32"            ${videos[*]}
+	runTest exiv2 -M "set Xmp.audio.SampleCount 32"           ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.DateUT $date"             ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Comment Metadata was Edited Using Exiv2" ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Language Kannada"         ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Country India"            ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Copyright Photographer"   ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Genre Sample Test Video"  ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Software Exiv2 0.25"      ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.Junk Its a junk Data"     ${videos[*]}
+	runTest exiv2 -M "set Xmp.video.MediaLanguage English"    ${videos[*]}
+	
+	for video in ${videos[*]}; do
+	    printf "." >&3
 
-    	    echo
-        	echo "-----> $video <-----"
+    	echo
+        echo "-----> $video <-----"
 
-    	    echo
-        	echo "Command: exiv2 -u -pa $video"
-	        runTest exiv2 -u -pa "$video"
-    	     exitcode="$?"
-        	echo "Exit code: $exitcode"
+    	echo
+        echo "Command: exiv2 -u -pa $video"
+	    runTest exiv2 -u -pa "$video"
+    	exitcode="$?"
+        echo "Exit code: $exitcode"
 
-	        if [ "$exitcode" -ne 0 -a "$exitcode" -ne 253 ] ; then
-    	        continue
-        	fi
-	fi
+	    if [ "$exitcode" -ne 0 -a "$exitcode" -ne 253 ] ; then
+    	    continue
+        fi
     done
 
-) 3>&1 > "$testdir/video-test.out" 2>&1
+) 3>&1 > "$out" 2>&1
 
 echo "."
 
 # ----------------------------------------------------------------------
 # Result
 
-diffCheck "$testdir/video-test.out" "$testdir/$datadir/write-video-test.out"
-
-	#Delete Temporary Directory 
-        rm $testdir/* -rf
+diffCheck "$out" "$testdir/$datadir/video/$this.out" 
 
 if [ $errors ]; then
-	red='\e[0;31m' #Display fonts in Red color
-	NC='\e[0m' # No Color
-	echo -e "${red}Test cases Failed because of Changes in the Source code or Test samples${NC}"
+	echo -e $errors 'test case(s) failed!'
 else
-	green='\e[0;32m' #Display fonts in Green color
-	NC='\e[0m' # No Color
-	echo -e "${green}All testcases passed.${NC}"
-	echo "That's all Folks!"
+	echo -e "all testcases passed."
 fi
 
 # That's all Folks!
