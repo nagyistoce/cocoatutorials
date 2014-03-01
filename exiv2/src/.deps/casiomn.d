859355badbfd4834e6350a363c16b1014e435073
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2014-03-01 23:25:25 +0000

    Rollback 941

---
+++ exiv2/src/.deps/casiomn.d
@@ -0,0 +1,4 @@
+casiomn.o: casiomn.cpp rcsid_int.hpp types.hpp exv_conf.h version.hpp \
+  casiomn_int.hpp tags.hpp metadatum.hpp value.hpp tags_int.hpp i18n.h
+casiomn.cpp rcsid_int.hpp types.hpp exv_conf.h version.hpp :
+  casiomn_int.hpp tags.hpp metadatum.hpp value.hpp tags_int.hpp i18n.h :
