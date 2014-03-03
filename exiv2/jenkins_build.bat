cd9d73623f931b96188c1c3da7f8ee0e5aeaaa68
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2014-03-03 19:41:03 +0000

    more polish for jenkins_build.bat

--- exiv2/jenkins_build.bat
+++ exiv2/jenkins_build.bat
@@ -25,6 +25,9 @@ if NOT DEFINED curl          set  curl=false
 if NOT DEFINED libssh        set  libssh=false
 if NOT DEFINED openssl       set  openssl=false
 
+if %ACTION%==/clean          set tests=false
+if %ACTION%==/upgrade        set tests=false
+
 if NOT DEFINED INCLUDE       set "INCLUDE=C:\Program Files (x86)\Microsoft Visual Studio 8\VC\ATLMFC\INCLUDE;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\INCLUDE;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\include;c:\Program Files (x86)\Microsoft Visual Studio .NET 2003\SDK\v1.1\include\;c:\home\rmills\dev\win32\boost\include\boost-1_42
 if NOT DEFINED LIB           set "LIB=C:\Program Files (x86)\Microsoft Visual Studio 8\VC\ATLMFC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\LIB;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\PlatformSDK\lib;C:\Program Files (x86)\Microsoft Visual Studio 8\SDK\v2.0\lib;c:\Program Files (x86)\Microsoft Visual Studio .NET 2003\SDK\v1.1\Lib\"
 if NOT DEFINED LIBPATH       set "LIBPATH=C:\Windows\Microsoft.NET\Framework\v2.0.50727;C:\Program Files (x86)\Microsoft Visual Studio 8\VC\ATLMFC\LIB"
@@ -39,12 +42,11 @@ rem Pull in support libraries
 if NOT EXIST ..\expat-2.1.0 xcopy/yesihq  c:\exiv2libs\expat-2.1.0 ..\expat-2.1.0
 if NOT EXIST ..\zlib-1.2.7  xcopy/yesihq  c:\exiv2libs\zlib-1.2.7  ..\zlib-1.2.7
 
-cd msvc2005
-
 rem --
 rem build and test
-if %ACTION%==/clean   set tests=false
-if %ACTION%==/upgrade set tests=false
+
+pushd msvc2005
+
 for /f "tokens=*" %%a in ('cygpath -au ..') do set FOO=%%a
 
 if %Win32%==true (
@@ -110,6 +112,8 @@ if %x64%==true   (
       if %tests%==true   (
         call bash -c 'cd %FOO%;cd test;./testMSVC.sh ../msvc2005/bin/x64/ReleaseDLL'
 ) ) ) )
+
+popd
 
 rem That's all Folks!
 rem -----------------
