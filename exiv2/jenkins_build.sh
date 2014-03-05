bb1b90794f4196f876f1b3b4372890f07bc8d55c
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2014-03-05 07:31:30 +0000

    updating jenkins_build.sh

--- exiv2/jenkins_build.sh
+++ exiv2/jenkins_build.sh
@@ -2,13 +2,25 @@
 
 export PATH=$PATH:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/bin:/usr/lib/pkgconfig:/opt/local/bin:$PWD/usr/bin:/opt/local/bin:/opt/local/sbin:/opt/pkgconfig:bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/usr/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
-make config
-./configure --prefix=$PWD/usr
+
+if [ "$ACTION" == /rebuild ]; then
+	if [ -e configure ]; then
+		make distclean;
+	fi
+fi
+
+if [ ! -e configure ]; then 
+	make config
+	./configure --prefix=$PWD/usr
+fi
+
 make
 make install
 make samples
-make tests
-export
+
+if [ "$tests" == true ]; then
+	make tests
+fi
 
 # That's all Folks!
 ##
