16128924908e51ce5a1b299a6ad142562ac67f7c
robinwmills@gmail.com <robinwmills@gmail.com@008a7418-851a-2591-e00c-8eeb78c5c594>
2014-08-19 10:17:43 +0000

    jenkins testing

--- exiv2/jenkins_build.sh
+++ exiv2/jenkins_build.sh
@@ -2,9 +2,7 @@
 
 set +v
 DIR="$PWD"
-#if [ "$HOME" == "/Users/rmills" ]; then 
-  source ~/bin/.profile
-#fi
+source ~/bin/.profile
 cd "$DIR"
 
 if [ -z "$tests" ]; then tests=true; fi
@@ -27,18 +25,42 @@ fi
 #done
 
 make config
-# if [ "$PLATFORM" == "cygwin" ]; then 
-#	export LIBS=-lintl
-#fi
 
 if [ "$PLATFORM" == "cygwin" ]; then 
 #	export LIBS=-lintl
-    # I've given up trying to build with gettext and friends on Cygwin
-    # And trying to get Cygwin to install into a local directory
-	./configure --disable-nls
-	make install
-	make
-	make samples
+    # I've given up:
+    # 1 trying to get Cygwin to build with gettext and friends
+    # 2 trying to get Cygwin to install into a local directory
+#	./configure --disable-nls
+#	make install
+#	make
+#	make samples
+
+	##
+	# Daisy chain to msvc Build
+	export ACTION=/rebuild
+	export BuildEnv=native
+	export Builder=2005
+	export COMPILER=G++
+	export debug=false
+	export dll=true
+	export expat=true
+	export libssh=false
+	export Linux=true
+	export MSVC=true
+	export openssl=false
+	export release=true
+	export static=false
+	export teste=false
+	export testr=false
+	export tests=true
+	export testv=false
+	export Win32=false
+	export x64=true
+	export zlib=true	
+
+	PATH=$PATH:/cygdrive/c/Windows/System32
+	cmd.exe /c "cd $(cygpath -aw .) && call jenkins_build.bat"
 else
 	./configure --prefix=$PWD/usr
 	make "LDFLAGS=-L${PWD}/usr/lib -L${PWD}/xmpsdk/src/.libs"
